3f8f2ed7d2f2c6b9d585cf0c05bd16dde3df23c1
Ali Uzair <au7731080@gmail.com>
2026-08-26 22:36:53 +0500

    Added line 34 and 40 for just simply processing 1 fame and skipping the next

--- Face_recognition.py
+++ Face_recognition.py
@@ -29,34 +29,40 @@ def encodings(images):
 list_encode = encodings(images)
 
 cap = cv2.VideoCapture(0)
-
+process_this_frame = True
 while True:
-    success, frame = cap.read()
-    imgS = cv2.resize(frame,(0,0),None,fx=0.25,fy=0.25)  #just for speeding the process
-    imgS = cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
-    frame_loc = fr.face_locations(imgS)
-    frame_encodings = fr.face_encodings(imgS,frame_loc)
+    if process_this_frame:
+        success, frame = cap.read()
+        imgS = cv2.resize(frame,(0,0),None,fx=0.25,fy=0.25)  #just for speeding the process
+        imgS = cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
+        frame_loc = fr.face_locations(imgS)
+        frame_encodings = fr.face_encodings(imgS,frame_loc)
+    process_this_frame = not process_this_frame
 
     for encoding, loc in zip(frame_encodings,frame_loc):
         matches = fr.compare_faces(list_encode,encoding)
         face_dis = fr.face_distance(list_encode,encoding)
         match_index = np.argmin(face_dis)
 
+        y1,x2,y2,x1 = loc
+        y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+
         if matches[match_index]: 
             name = img_names[match_index].upper()
-            y1,x2,y2,x1 = loc
-            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+            
             cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
             cv2.rectangle(frame,(x1,y2+30),(x2,y2),(0,255,0),cv2.FILLED)
             cv2.putText(frame,name,(x1+6,y2+25),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
         else:
             name = "Unknown"
-            y1,x2,y2,x1 = loc
-            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
             cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),2)
             cv2.rectangle(frame,(x1,y2+30),(x2,y2),(0,0,255),cv2.FILLED)
             cv2.putText(frame,name,(x1+6,y2+25),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
     cv2.imshow("Webcam",frame)
-    cv2.waitKey(1)    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()  
    
    
